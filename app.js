be250a609fa2a91d2c7ef2b3e5d6b2fd5177d1e6
Ayaz <ayaz18164@gmail.com>
2023-10-24 00:38:06 +0500

    Modal

---
+++ app.js
@@ -0,0 +1,14 @@
+const modalProject = document.getElementById("modalProject")
+const modalProject2 = document.getElementById("modalProject2")
+
+
+function openModal(){
+modalProject.style.display = ("none")
+modalProject2.style.display = ("block")
+}
+
+
+function openModal2() {
+    modalProject.style.display = ("block")
+modalProject2.style.display = ("none")
+}
